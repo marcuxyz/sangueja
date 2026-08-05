73fd4ec540b787183b03808dc6ceb8cecd3f371e
Marcus Almeida <mpereirassa@gmail.com>
2026-08-05 19:16:09 -0300

    Refatora CrawlerBase para melhorar a implementação do Template Method

--- app/crawlers/crawler_base.py
+++ app/crawlers/crawler_base.py
@@ -1,16 +1,73 @@
 from abc import ABC, abstractmethod
+
+from bs4 import BeautifulSoup
+from sqlalchemy import text
+from sqlalchemy import create_engine
+
 from app.http.client import HttpClient
 
 
 class CrawlerBase(ABC):
-    def __init__(self):
-        self.response_text = self.perform()
+    def __init__(self, autostart: bool = True):
+        self.data = None
+
+        if autostart:
+            self.data = self.payload()
 
     def perform(self):
-        client = HttpClient(url=self.default_url())
-        response = client.download_html()
-        return response.text
+        return self.parse(self.document())
+
+    def save(self, data: dict) -> bool:
+        """Inserts a new blood center into the database.
+        Args:
+            name (str): The name of the blood center.
+            city (str): The city where the blood center is located.
+            state (str): The state abbreviation (automatically converted to uppercase).
+
+        Returns:
+            bool: True if the insertion was successful, False otherwise.
+        """
+        engine = create_engine("sqlite:///databases/test.sqlite")
+        with engine.begin() as conn:
+            conn.execute(
+                text("""
+                    INSERT INTO blood_centers (name, city, state)
+                    VALUES (:name, :city, :state)
+                """),
+                {
+                    "name": data["name"],
+                    "city": data["city"],
+                    "state": data["state"],
+                },
+            )
+
+        return True
+
+    def download_page(self):
+        client = HttpClient(url=self.target_url())
+
+        return client.download_html()
+
+    def parse(self, docuemnt):
+        parser = self.parser_class()
+
+        return parser.parse(docuemnt)
+
+    def document(self):
+        page = self.download_page()
+
+        return BeautifulSoup(page.text, "html.parser")
+
+    def payload(self):
+        return self.blood_center() | self.perform()
+
+    def parser_class(self):
+        raise NotImplementedError("#{self.class} must define #parser_class")
 
     @abstractmethod
-    def default_url(self):
-        raise NotImplementedError("Subclasses must implement the default_url method.")
+    def target_url(self):
+        raise NotImplementedError("Subclasses must implement the target_url method.")
+
+    @abstractmethod
+    def blood_center(self):
+        raise NotImplementedError("Subclasses must implement the blood_center method.")
