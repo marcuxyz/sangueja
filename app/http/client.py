7fc7501a2eb3862d2792ee62fbee0b68a1d87710
Marcus Almeida <mpereirassa@gmail.com>
2026-09-05 16:02:13 -0300

    refactor: ajusta cliente HTTP
    - Remove método de inicialização do método
    - Refatora download_html para usar cliente httpx

--- app/http/client.py
+++ app/http/client.py
@@ -1,3 +1,4 @@
+import os
 import httpx
 
 
@@ -8,9 +9,20 @@ class HttpClient:
         url (str, optional): Pass the URL to connect via HTTP protocol. Defaults to None.
     """
 
-    def __init__(self, url: str = None, method="GET"):
+    def __init__(self, url: str = None):
         self.url = url
-        self.method = method
+
+
+    def client(self):
+        return httpx.Client(
+            timeout=self.timeout()
+        )
+
+    def timeout(self):
+        return httpx.Timeout(
+            timeout=os.getenv("HTTP_TIMEOUT", 30),
+            connect=os.getenv("HTTP_CONNECT_TIMEOUT", 45)
+        )
 
     def download_html(self) -> str:
         """Responsible for download HTML from URL
@@ -22,6 +34,18 @@ class HttpClient:
             str: The downloaded HTML content.
         """
         try:
-            return httpx.get(self.url)
+
+            response = self.client().get(
+                self.url,
+
+                timeout=30,
+                headers={
+                    "User-Agent": "SangueJá/1.0 (+https://whatsapp.com/channel/0029VbDs7Jv47XeJnANDSG3l)"
+                },
+            )
+
+            response.raise_for_status()
+
+            return response.text
         except httpx.HTTPError as error:
-            raise RuntimeError("failed to download #{error}") from error
+            raise RuntimeError(f"failed to download #{error}") from error
