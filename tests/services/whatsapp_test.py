dbba5c60948cc48d641bae1861e447a9d94a25e1
Marcus Almeida <mpereirassa@gmail.com>
2026-09-11 15:49:19 -0300

    feat: ajusta teste WhatsApp - Adiciona variáveis de ambiente no teste - Remove URL fixa do cliente WhatsApp

--- tests/services/whatsapp_test.py
+++ tests/services/whatsapp_test.py
@@ -3,11 +3,16 @@ from app.services.whatsapp import Whatsapp
 
 
 @mock.patch("app.services.whatsapp.Whatsapp.send_notification")
-def test_send_notification(mock_send_notification):
+def test_send_notification(mock_send_notification, monkeypatch):
     mock_send_notification.return_value = True
+    monkeypatch.setenv("WHATSAPP_URL", 'https://test.whatsapp')
+    monkeypatch.setenv("WHATSAPP_TOKEN", '9A8897CGS7')
+    monkeypatch.setenv("WHATSAPP_NUMBER", '719899999999')
 
-    whats = Whatsapp(url="http://localhost")
-    response = whats.send_notification(message="Congratz my friend")
+    whatsapp = Whatsapp()
+    response = whatsapp.send_notification(message="Congratz my friend")
 
     assert response == True
-    mock_send_notification.assert_called_once_with(message="Congratz my friend")
+    mock_send_notification.assert_called_once_with(
+        message="Congratz my friend"
+    )
