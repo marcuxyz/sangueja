69eee8b31facee743466fed8a9aedb6258543df7
Marcus Vinícius <mpereirassa@gmail.com>
2026-09-17 04:28:08 -0300

    feat: Adiciona tipos de sangue ALERTA (#30)
    * feat: Adiciona tipos de sangue ALERTA
    - Adiciona tipo de sangue que estão em alerta
    - Modifica mensagem no whatsapp com o novo tipo de sangue adicionado
    
    * feat: ajusta alerta de tipos sanguíneos
    
    - Refatora lógica de status dos tipos sanguíneos
    - Melhora a legibilidade do código no template
    
    * refatora alerta de hemocentros
    - Remove cidade e endereço do alerta
    - Mantém informações essenciais
    
    * refatora teste de crawler
    
    - Renomeia função de teste
    - Remove transações desnecessárias

--- tests/crawler/hemoba/crawler_test.py
+++ tests/crawler/hemoba/crawler_test.py
@@ -17,20 +17,14 @@ def test_base_perform_is_called_once_times(mock_perform):
 
 @patch.object(Crawler, "send_alert")
 @patch("app.crawlers.base.HttpClient.download_html")
-def test_return_data_of_database_test(
+def test_return_data_of_database(
     download_html, mock_send_alert, hemoba_html, monkeypatch
 ):
     conn = Connection().connect()
-    transaction = Transaction(conn)
     download_html.return_value.text = hemoba_html
     monkeypatch.setenv("WHATSAPP_URL", "https://test.whatsapp")
     monkeypatch.setenv("WHATSAPP_TOKEN", "9A8897CGS7")
     monkeypatch.setenv("WHATSAPP_NUMBER", "719899999999")
-
-    transaction.create_all()
-    for blood_type in ("A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"):
-        transaction.insert_blood_type(blood_type)
-    conn.commit()
 
     crawler = Crawler()
     first_result = crawler.perform()
@@ -51,7 +45,24 @@ def test_return_data_of_database_test(
     assert blood_stock_items == 8
     assert second_result == first_result
 
-    transaction.drop_all()
+
+@patch("app.crawlers.base.HttpClient.download_html")
+def test_return_critical_blood_types(download_html, hemoba_html):
+    download_html.return_value.text = hemoba_html
+    crawler = Crawler()
+
+    crawler.perform()
+
+    warning_blood_types = list(crawler.filter_warning_blood_types())
+
+    assert warning_blood_types == [
+        {"name": "A+", "status": "Alerta"},
+        {"name": "A-", "status": "Alerta"},
+        {"name": "B+", "status": "Alerta"},
+        {"name": "B-", "status": "Crítico"},
+        {"name": "O+", "status": "Crítico"},
+        {"name": "O-", "status": "Crítico"},
+    ]
 
 
 def test_crawler_uses_source_url_from_environment(monkeypatch):
