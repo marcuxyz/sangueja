e65eba7946498946de3cd563bb696d16da407553
Marcus Vinícius <mpereirassa@gmail.com>
2026-09-22 11:12:20 -0300

    feat: Atualiza crawler para hemoes (#41)
    * feat: Atualiza crawler para hemoes
    
    - Adiciona suporte ao hemoes
    - Permite execução via argumento
    
    * feat: Atualiza nome e comandos do crawler
    
    - Renomeia o workflow para "Crawler run"
    - Ajusta comando de execução do crawler

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+import argparse
+
+from app.crawlers.hemoes.crawler import HemoesCrawler
 from app.crawlers.hemoba.crawler import HemobaCrawler
 
+
+CRAWLERS = {
+    "hemoba": HemobaCrawler,
+    "hemoes": HemoesCrawler
+}
+
+
 def main():
-    crawl = HemobaCrawler()
-    crawl.execute()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--bloodcenter",
+        choices=CRAWLERS.keys(),
+        help="Executa deteminado crawler de acordo com o valor passado"
+    )
+    args = parser.parse_args()
+
+    if args.bloodcenter:
+        crawler = CRAWLERS[args.bloocenter]()
+        crawler.execute()
+        return
+
+    for crawler in CRAWLERS.values():
+        crawl = crawler()
+        crawl.execute()
+
 
 if __name__ == "__main__":
     main()
